af9d3e599756b1955c7255cf186da6b2f42572f4
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-07-13 15:35:02 +0200

    in progress

--- routes/api.js
+++ routes/api.js
@@ -11,5 +11,34 @@ module.exports = function (app) {
     // TODO
     const stock = req.query.stock;
     const like = req.query.like;
+    if (!stock) {
+      res.send({ error: "No required stock field" });
+      return;
+    }
+    if (typeof stock === "string") {
+      axios
+        .get(
+          `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+        )
+        .then((foundStock) => {})
+        .catch((error) => {
+          res.send({ error: error });
+        });
+      return;
+    }
+    if (Array.isArray(stock)) {
+      axios.all
+        .get(
+          `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock[0]}/quote`
+        )
+        .get(
+          `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock[1]}/quote`
+        )
+        .then((stock1, stock2) => {})
+        .catch((error) => {
+          res.send({ error: error });
+        });
+      return;
+    }
   });
 };
